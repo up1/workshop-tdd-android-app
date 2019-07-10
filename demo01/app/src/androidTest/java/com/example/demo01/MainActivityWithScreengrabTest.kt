01f2ccba6bd822e2bcbcca0de02a439aebd0cab4
up1 <somkiat.p@gmail.com>
2019-07-10 09:18:38 +0700

    Add screengrab

---
+++ demo01/app/src/androidTest/java/com/example/demo01/MainActivityWithScreengrabTest.kt
@@ -0,0 +1,31 @@
+package com.example.demo01
+
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.support.test.espresso.matcher.ViewMatchers.withText
+import android.support.test.rule.ActivityTestRule
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+import tools.fastlane.screengrab.locale.LocaleTestRule
+import org.junit.ClassRule
+import tools.fastlane.screengrab.Screengrab
+
+
+class MainActivityWithScreengrabTest {
+
+    @get:ClassRule
+    @JvmField
+    val localeTestRule = LocaleTestRule()
+
+    @get:Rule
+    val activityRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test fun open_main_page() {
+        Screengrab.screenshot("step_01")
+        onView(withText("Hello World!"))
+            .check(matches(isDisplayed()))
+    }
+
+}
