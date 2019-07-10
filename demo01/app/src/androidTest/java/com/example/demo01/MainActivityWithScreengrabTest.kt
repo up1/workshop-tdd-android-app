7f349782e705ecb7423dfb48591f732f78cc786a
up1 <somkiat.p@gmail.com>
2019-07-10 13:19:11 +0700

    Capture with screengrab

--- demo01/app/src/androidTest/java/com/example/demo01/MainActivityWithScreengrabTest.kt
+++ demo01/app/src/androidTest/java/com/example/demo01/MainActivityWithScreengrabTest.kt
@@ -11,6 +11,9 @@ import org.junit.Test
 import tools.fastlane.screengrab.locale.LocaleTestRule
 import org.junit.ClassRule
 import tools.fastlane.screengrab.Screengrab
+import tools.fastlane.screengrab.UiAutomatorScreenshotStrategy
+
+
 
 
 class MainActivityWithScreengrabTest {
@@ -23,6 +26,7 @@ class MainActivityWithScreengrabTest {
     val activityRule = ActivityTestRule(MainActivity::class.java)
 
     @Test fun open_main_page() {
+        Screengrab.setDefaultScreenshotStrategy(UiAutomatorScreenshotStrategy())
         Screengrab.screenshot("step_01")
         onView(withText("Hello World!"))
             .check(matches(isDisplayed()))
